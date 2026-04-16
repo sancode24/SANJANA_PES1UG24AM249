2f06bf59fbbe36ae8d23185fb4fc30dfac02046a
sancode24 <sanjanamkulkarni25@gmail.com>
2026-04-16 11:07:45 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -10,6 +10,8 @@
 //   "100644 hello.txt\0" followed by 32 raw bytes of SHA-256
 
 #include "tree.h"
+#include "pes.h"
+#include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
